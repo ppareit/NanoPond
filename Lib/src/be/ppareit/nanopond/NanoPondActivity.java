f2b4684051635f92b23c7e761602fd25f41ca552
Pieter Pareit <pieter.pareit@gmail.com>
2013-02-14 17:10:42 +0100

    Target android sdk 17

--- Lib/src/be/ppareit/nanopond/NanoPondActivity.java
+++ Lib/src/be/ppareit/nanopond/NanoPondActivity.java
@@ -73,10 +73,8 @@ public class NanoPondActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.action_editcell:
+        if (item.getItemId() == R.id.action_editcell) {
             showDialog(DIALOG_EDITCELL);
-            break;
         }
         return true;
     }
