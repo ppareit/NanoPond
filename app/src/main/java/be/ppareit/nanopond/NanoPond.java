960f6c676c4d9b5086beb48722ab165b315248a6
Pieter Pareit <pieter.pareit@gmail.com>
2016-01-02 11:35:25 +0100

    Give the device some breathing space when running

--- app/src/main/java/be/ppareit/nanopond/NanoPond.java
+++ app/src/main/java/be/ppareit/nanopond/NanoPond.java
@@ -290,6 +290,11 @@ public class NanoPond {
             @Override
             public void run() {
                 while (true) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     singleStep();
                 }
             }
