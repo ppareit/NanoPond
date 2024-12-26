649586c3e46a7fcd0bec64f6d2dfcac1f212fff6
Pieter Pareit <pieter.pareit@gmail.com>
2024-12-26 09:18:54 +0100

    Update and upgrade libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "be.ppareit.nanopond"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "be.ppareit.nanopond"
         minSdk = 26
-        targetSdk = 34
+        //targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -44,8 +44,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    // testImplementation(libs.junit)
-    // androidTestImplementation(libs.androidx.junit)
-    // androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
 }
